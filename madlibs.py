1e005b64aa5e625cb8cf01ee2129ee3169842924
Riza May Largadas <138735214+rizalargadas@users.noreply.github.com>
2024-03-07 18:00:21 -0500

    renamed randomizer

--- madlibs.py
+++ madlibs.py
@@ -8,41 +8,41 @@ verb_2 = input("Verb: ")
 verb_3 = input("Verb: ")
 person = input("Famous Person: ")
 
-randomomizer = random.randint(1, 10)
+randomizer = random.randint(1, 10)
 
-if randomomizer == 1:
+if randomizer == 1:
     print(stories.madlibs1.format(adj_1=adj_1, adj_2=adj_2, verb_1=verb_1,
                                   verb_2=verb_2, verb_3=verb_3, person=person))
 
-elif randomomizer == 2:
+elif randomizer == 2:
     print(stories.madlibs2.format(adj_1=adj_1, adj_2=adj_2, verb_1=verb_1,
                                   verb_2=verb_2, verb_3=verb_3, person=person))
 
-elif randomomizer == 3:
+elif randomizer == 3:
     print(stories.madlibs3.format(adj_1=adj_1, adj_2=adj_2, verb_1=verb_1,
                                   verb_2=verb_2, verb_3=verb_3, person=person))
 
-elif randomomizer == 4:
+elif randomizer == 4:
     print(stories.madlibs4.format(adj_1=adj_1, adj_2=adj_2, verb_1=verb_1,
                                   verb_2=verb_2, verb_3=verb_3, person=person))
 
-elif randomomizer == 5:
+elif randomizer == 5:
     print(stories.madlibs5.format(adj_1=adj_1, adj_2=adj_2, verb_1=verb_1,
                                   verb_2=verb_2, verb_3=verb_3, person=person))
 
-elif randomomizer == 6:
+elif randomizer == 6:
     print(stories.madlibs6.format(adj_1=adj_1, adj_2=adj_2, verb_1=verb_1,
                                   verb_2=verb_2, verb_3=verb_3, person=person))
 
-elif randomomizer == 7:
+elif randomizer == 7:
     print(stories.madlibs7.format(adj_1=adj_1, adj_2=adj_2, verb_1=verb_1,
                                   verb_2=verb_2, verb_3=verb_3, person=person))
 
-elif randomomizer == 8:
+elif randomizer == 8:
     print(stories.madlibs8.format(adj_1=adj_1, adj_2=adj_2, verb_1=verb_1,
                                   verb_2=verb_2, verb_3=verb_3, person=person))
 
-elif randomomizer == 9:
+elif randomizer == 9:
     print(stories.madlibs9.format(adj_1=adj_1, adj_2=adj_2, verb_1=verb_1,
                                   verb_2=verb_2, verb_3=verb_3, person=person))
 
